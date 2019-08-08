2a144298d8ce84fa50719c4eb8a9c9d746c905f6
autumn-ragland <raglanda998@gmail.com>
2019-08-08 12:21:43 -0500

    Complete

---
+++ index.js
@@ -0,0 +1,21 @@
+// ex 1
+// Print "Hello World"
+console.log("Hello World");
+
+// ex 2
+// Create a variable called nameVariable and assign it your name.
+// Print "Hello " + nameVariable
+
+var nameVariable = "Autumn";
+console.log("Hello " + nameVariable);
+
+// ex 3
+// Create a variable named dogAge and assign it an integer.
+//  Print "Your dog is: " + dogAge * 7
+
+var dogAge = 5;
+console.log("Your dog is: " + (dogAge*7));
+
+// ex 4
+// Print "1 + 1 = " and the answer.
+console.log("1 + 1 = " + (1+1));
